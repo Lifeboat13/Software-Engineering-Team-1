dc8389346fed67b81b9fb597c75c559c92652f77
Gregory J. Friedman <gfried5@students.towson.edu>
2015-04-25 16:50:34 -0400

    Removed old files

--- AirTrafficControlLCMS/src/Auditor.java
+++
@@ -1,4 +0,0 @@
-
-public class Auditor extends User{
-
-}
--- AirTrafficControlLCMS/src/Driver.java
+++
@@ -1,7 +0,0 @@
-
-public class Driver {
-	public static void main(String[] args){
-		GUI gui = GUI.getInstance();
-		gui.displaySimulator();
-	}
-}
